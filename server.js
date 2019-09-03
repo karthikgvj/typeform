37635fe10959013820c9367495569a72911b7f51
karthikgvj <karthikrajaed@gmail.com>
2019-09-03 15:06:06 +0530

    server.js updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/typeform'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('index.html'));
+res.sendFile(path.join(__dirname + '/dist/typeform/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
